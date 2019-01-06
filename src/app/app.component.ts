83e5a181facf50673c20bdb509f67475dbd23f9e
Sanjeev96 <sanjeevdhillon12@hotmail.co.uk>
2019-01-06 17:01:23 +0000

    udemy course for property and event binding
    game-control component task to have start button that auto increments numbers and a stop to stop the increment
    odd and even component - Pass number using Input
    app component - display components and use output directive to emit variable for incrementing number be passed as a paramter inside app component function that displays numbers as odd or even.
    
    First I go the number to display inside game-control component as a number (nto facotring odd or even ) just a single number variable
    Formatted components for view
    Placed input variable in odd and even
    created  event binding output to emit the number variable
    used emitted variable for calcualting even and odd by passing it as a parameter ($event) into app component function
    used .push() to push parameter to an even/odd array
    ngfor for displaying each incremented number

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'practiceComponents';
+
+  public oddNumberArray: number[] = []; // param from emiiter in gamecontrol pushed into this array
+  public evenNumberArray: number[] = [];
+
+
+  ontimeFired(firedNum: number) {
+    if (firedNum % 2 == 0) {
+      this.evenNumberArray.push(firedNum);
+    } else {
+      this.oddNumberArray.push(firedNum);
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { GameControlComponent } from './game-control/game-control.component';
+import { OddComponent } from './odd/odd.component';
+import { EvenComponent } from './even/even.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SuccessAlertComponent,
-    WarningAlertComponent
+    GameControlComponent,
+    OddComponent,
+    EvenComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
